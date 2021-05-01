7c985792aba9839b0e789e302c270cd0c1b235cc
Avagyan7 <yuri.avagyan1995@gmail.com>
2021-05-01 20:37:21 +0400

    thirdchange

---
+++ lesson 2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+//int count = 7;
+//cout<<count;
+int age;
+int count;
+
+cout<< "Enter a count ";
+cin >> count;
+cout<< "Enter a age ";
+cin>>age;
+
+cout << "Count = " << count <<endl;
+cout << "Age = " << age << endl; 
+return 0;
+}
